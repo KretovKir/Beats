1763990a3806c03db52a6cb800d81d098da636c3
Kirill Kretov <kretovkir526@mail.ru>
2023-10-19 16:29:39 +0300

    added slider

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,21 +1,22 @@
 const leftArrow = document.querySelector('#leftArrow');
 const rightArrow = document.querySelector('#rightArrow');
-const slider = document.querySelector('.slider');
-const list = document.querySelector('.slider__items');
+const slider = document.querySelector('#slider');
+const list = document.querySelector('#slider__items');
 var currentPosition = 0;
 
 leftArrow.addEventListener('click', function(event){
     event.preventDefault();
-    if(currentPosition>0){
+    if (currentPosition>0){
         currentPosition-=940;
-        list.style.right = `rem(${currentPosition}px)`;
+        list.style.right = `${currentPosition}px`;
     }
-})
+});
 
 rightArrow.addEventListener('click', function(event){
     event.preventDefault();
-    if(currentPosition<1880){
+    
+    if (currentPosition<1880){
         currentPosition+=940;
-        list.style.right = `rem(${currentPosition}px)`;
+        list.style.right = `${currentPosition}px`;
     }
 })
