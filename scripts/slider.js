9f9e50d0ac37480c92ffb5073af1bdf102efbcfc
Kirill Kretov <kretovkir526@mail.ru>
2023-10-23 15:00:43 +0300

    fix slider code

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,21 +2,36 @@ const leftArrow = document.querySelector('#leftArrow');
 const rightArrow = document.querySelector('#rightArrow');
 const slider = document.querySelector('#slider');
 const list = document.querySelector('#slider__items');
-var currentPosition = 0;
+const sliderItem = document.querySelector('#slider__item');
+const computedStyles = getComputedStyle(list);
+const computedStylesWidth = getComputedStyle(sliderItem);
+const sliderWidth = getComputedStyle(slider);
 
 leftArrow.addEventListener('click', function(event){
     event.preventDefault();
-    if (currentPosition>0){
-        currentPosition-=940;
+    let currentPosition = parseInt(computedStyles.right);
+    let currentWidth = parseInt(computedStylesWidth.width);
+    let currentSliderWidth = parseInt(sliderWidth.width);
+    if (currentPosition>currentWidth){
+        list.style.right = `${currentPosition - currentWidth}px`;
+        console.log(currentSliderWidth);
+    }else{
+        currentPosition = 940;
         list.style.right = `${currentPosition}px`;
     }
 });
 
 rightArrow.addEventListener('click', function(event){
+    let currentPosition = parseInt(computedStyles.right);
+    let currentWidth = parseInt(computedStylesWidth.width);
     event.preventDefault();
     
-    if (currentPosition<1880){
-        currentPosition+=940;
+    if (currentPosition<currentWidth){
+        list.style.right = `${currentPosition + currentWidth}px`;
+        console.log(currentWidth);
+    }else{
+        currentPosition = 0;
         list.style.right = `${currentPosition}px`;
+        console.log(currentWidth);
     }
 })
